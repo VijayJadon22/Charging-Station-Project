be004c645edda0f17c4d99078df0d3c6f25afe18
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-01 23:29:22 +0530

    Implemnted signup controller function for signing up user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -27,15 +27,44 @@ export const login = async (req, res) => {
         });
 
     } catch (error) {
-        console.log("Error in login controller function: ", error);
+         // Log any errors to the console
+        console.error("Error in login controller: ", error);
+        // Send a 500 Internal Server Error response in case of an exception
+        return res.status(500).json({ error: "Internal server error" });
     }
 }
 
 export const signup = async (req, res) => {
     try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
 
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+        const newUser = new User({
+            name,
+            email,
+            password
+        });
+
+        if (newUser) {
+            generateTokenAndSetCookie(newUser, res);
+            await newUser.save();
+
+            return res.status(200).json({
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email
+            })
+        }else {
+            return res.status(400).json({ message: "Invalid user data" });
+        }
     } catch (error) {
-
+        console.error("Error in signup controller: ", error.message); // Log the error to the console
+        return res.status(500).json({ error: "Internal server error" }); // Respond with a 500 status code and error message
     }
 }
 
